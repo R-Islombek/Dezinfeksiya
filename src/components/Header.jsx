72ca0c1418eb96f77576960ad9243c44e30cf1a2
R-Islombek <islombekraximjanov97dev@gmail.com>
2026-03-04 10:08:16 +0500

    2-Tailwind.css

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from "./images/logo.png"
+const Header = () => {
+  return (
+     <header className='header '>
+          <div>
+               <img src={Logo} alt="" />
+               <ul>
+                   <li></li>
+               </ul>
+          </div>
+     </header>
+  )
+}
+
+export default Header
