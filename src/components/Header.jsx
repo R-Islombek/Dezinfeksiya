04dd2f60e80df79a417f3c085d1fd3f3bd3b3146
R-Islombek <islombekraximjanov97dev@gmail.com>
2026-03-06 23:36:27 +0500

    header bo'limi tugadi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
-import Logo from "./images/logo.png"
+import Logo from "../components/images/logo.png"
+
 const Header = () => {
+
+  const links = [
+    { id: 1, name: "Services" },
+    { id: 2, name: "About" },
+    { id: 3, name: "Faq" },
+    { id: 4, name: "Contact" }
+  ]
+
   return (
-     <header className='header '>
-          <div>
-               <img src={Logo} alt="" />
-               <ul>
-                   <li></li>
-               </ul>
-          </div>
-     </header>
+    <header>
+      <div className='container flex items-center justify-between' >
+        <a href="#">
+          <img src={Logo} alt="logo" />
+        </a>
+
+        <ul className='flex gap-7.5' >
+          {links.map((link) => (
+            <li key={link.id}>
+              <a href="#">{link.name}</a>
+            </li>
+          ))}
+        </ul>
+        <a className='py-[5px] px-[60px] bg-[#3360FF]' href="#">Kirish</a>
+
+      </div>
+    </header>
   )
 }
 
